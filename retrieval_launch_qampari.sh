15ae53bcd9c4c272bb1ae93a29b073134abae189
Hung-Ting Chen <hc3337@torch-login-0.hpc.nyu.edu>
2026-01-22 08:44:51 -0500

    infer

--- retrieval_launch_qampari.sh
+++ retrieval_launch_qampari.sh
@@ -9,5 +9,5 @@ python search_r1/search/retrieval_server.py --index_path $index_file \
                                             --topk 3 \
                                             --retriever_name $retriever_name \
                                             --retriever_model $retriever_path \
-                                            --faiss_gpu \
-                                            --pooling_method mean
+                                            --pooling_method mean \
+					    --faiss_gpu
