97f2989b0bef87a15939b59e9d350348fa087d6e
timchen0618 <chen87618@gmail.com>
2026-01-28 08:53:38 -0500

    add timing

--- compute_inference_time.py
+++ compute_inference_time.py
@@ -13,18 +13,33 @@ def compute_inference_time(data):
         total_retrieval_time += item['total_retrieval_time']
     return total_inference_time, total_retrieval_time
 
+def compute_number_of_calls(data):
+    total_number_of_calls = []
+    for item in data:
+        total_number_of_calls.append(item['trajectory'].split('<answer> Beijing </answer>')[-1].count('<search>'))
+    return sum(total_number_of_calls) / len(total_number_of_calls)
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_file', type=str, default='output_infly.jsonl')
     args = parser.parse_args()
 
     data = read_jsonl(args.input_file)
-    total_inference_time, total_retrieval_time = compute_inference_time(data)
-    print(f"Total inference time: {total_inference_time} seconds")
-    print(f"Total retrieval time: {total_retrieval_time} seconds")
-    
-    print(f"Average inference time: {total_inference_time / len(data)} seconds")
-    print(f"Average retrieval time: {total_retrieval_time / len(data)} seconds")
+    try:
+        total_inference_time, total_retrieval_time = compute_inference_time(data)
+        print(f"Total inference time: {total_inference_time} seconds")
+        print(f"Total retrieval time: {total_retrieval_time} seconds")
+        
+        print(f"Average inference time: {total_inference_time / len(data)} seconds")
+        print(f"Average retrieval time: {total_retrieval_time / len(data)} seconds")
+    except:
+        print(f"Error computing inference time for {args.input_file}")
+        
+    try:
+        number_of_calls = compute_number_of_calls(data)
+        print(f"Average number of calls: {number_of_calls}")
+    except:
+        print(f"Error computing number of calls for {args.input_file}")
 
 if __name__ == '__main__':
     main()
