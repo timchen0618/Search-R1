c78ff726794524c136048318c5a03257d0eb3015
timchen0618 <chen87618@gmail.com>
2026-01-30 16:11:51 -0500

    multihop training

--- scripts/multihop_training/data_process.sh
+++ scripts/multihop_training/data_process.sh
@@ -2,7 +2,7 @@ WORK_DIR=/scratch/hc3337/projects/Search-R1
 LOCAL_DIR=$WORK_DIR/data/multihop_training
 
 ## process multiple dataset search format train file
-DATA=hotpotqa,2wikimultihopqa,musique,bamboogle
+DATA=hotpotqa,2wikimultihopqa,musique
 python $WORK_DIR/scripts/data_process/qa_search_train_merge.py --local_dir $LOCAL_DIR --data_sources $DATA
 
 ## process multiple dataset search format test file
