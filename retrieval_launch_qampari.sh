784024cadb357934e34f9565227700f90198e73a
timchen0618 <chen87618@gmail.com>
2026-02-12 13:12:27 -0500

    add agentic qampari data

--- retrieval_launch_qampari.sh
+++ retrieval_launch_qampari.sh
@@ -35,4 +35,4 @@ python search_r1/search/retrieval_server.py --index_path $index_file \
                                             --retriever_model $retriever_path \
                                             --pooling_method $pooling_method \
 					                        --faiss_gpu \
-                                            --port $1
+                                            --port $2
