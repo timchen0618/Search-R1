be9a872f5eb97c43e19f5b98172f64b00b1b4201
timchen0618 <chen87618@gmail.com>
2026-02-12 16:04:23 -0500

    modify script for bcp

--- scripts/bcp/data_process.sh
+++ scripts/bcp/data_process.sh
@@ -1,14 +1,12 @@
 WORK_DIR=/scratch/hc3337/projects/Search-R1
 
 ## process multiple dataset search format train file
-DATA=bcp
 LOCAL_DIR=$WORK_DIR/data/bcp_train_test_split
-python $WORK_DIR/scripts/data_process/bcp_search.py --local_dir $LOCAL_DIR --data_sources $DATA --template_type bcp --train_test_split
-python $WORK_DIR/scripts/data_process/bcp_search.py --local_dir $LOCAL_DIR --data_sources $DATA --template_type base --train_test_split
+python $WORK_DIR/scripts/data_process/bcp_search.py --local_dir $LOCAL_DIR --template_type bcp --train_test_split
+python $WORK_DIR/scripts/data_process/bcp_search.py --local_dir $LOCAL_DIR --template_type base --train_test_split
 
 
 ## process multiple dataset search format test file
-DATA=bcp
 LOCAL_DIR=$WORK_DIR/data/bcp_test
-python $WORK_DIR/scripts/data_process/bcp_search.py --local_dir $LOCAL_DIR --data_sources $DATA --template_type bcp
-python $WORK_DIR/scripts/data_process/bcp_search.py --local_dir $LOCAL_DIR --data_sources $DATA --template_type base
+python $WORK_DIR/scripts/data_process/bcp_search.py --local_dir $LOCAL_DIR --template_type bcp
+python $WORK_DIR/scripts/data_process/bcp_search.py --local_dir $LOCAL_DIR --template_type base
