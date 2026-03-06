59d7a44e79fa5cd0c3b97805f74391d853ae6a28
timchen0618 <tchen87618@gmail.com>
2026-03-06 14:47:40 -0500

    add rollout config to the files

--- verl/workers/rollout/vllm_rollout/vllm_rollout.py
+++ verl/workers/rollout/vllm_rollout/vllm_rollout.py
@@ -97,7 +97,9 @@ class vLLMRollout(BaseRollout):
                                     gpu_memory_utilization=config.gpu_memory_utilization,
                                     skip_tokenizer_init=False,
                                     max_model_len=config.prompt_length + config.response_length,
-                                    load_format=config.load_format)
+                                    load_format=config.load_format,
+                                    max_num_batched_tokens=config.get('max_num_batched_tokens', None),
+                                    max_num_seqs=config.get('max_num_seqs', None))
 
         # Offload vllm model to reduce peak memory usage
         self.inference_engine.offload_model_weights()
